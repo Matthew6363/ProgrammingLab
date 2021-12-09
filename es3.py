230ac7636d8e85c3258a7a1688dc0132059c934d
Mattia  Simonutti <94392498+Matthew6363@users.noreply.github.com>
2021-12-09 11:52:50 +0000

    es creazione lista di liste

---
+++ es3.py
@@ -0,0 +1,26 @@
+class CSVfile():
+    def __init__(self, name):
+
+        self.name = name
+
+    def get_data(self):
+
+        my_list = []
+
+        my_file = open(self.name, 'r')
+
+        for line in my_file:
+
+            riga = line.split(',')
+
+            if riga [0] != 'Date':
+                
+                riga[1] = riga[1][0:-1]
+                
+                my_list.append(riga)
+
+              
+        return my_list
+
+file1 = CSVfile('shampoo_sales.csv')
+print('Lista: {}'.format(file1.get_data()))
