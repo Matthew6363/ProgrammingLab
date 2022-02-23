88eadc0a3e3c21934e071f11624aca3b21d7b899
Mattia  Simonutti <94392498+Matthew6363@users.noreply.github.com>
2022-02-23 15:51:49 +0000

    es 6 ok

--- es6.py
+++ es6.py
@@ -1,6 +1,9 @@
+import datetime
 
-class ExamException:
+class ExamException(Exception):
     pass
+
+
 
 class CSVTimeSeriesFile():
     def __init__(self, name):
@@ -18,6 +21,9 @@ class CSVTimeSeriesFile():
             except Exception:
 
                 raise ExamException ('Non è stato possibile effettuare la conversione')
+
+  
+
 
     def get_data(self):
 
@@ -41,60 +47,58 @@ class CSVTimeSeriesFile():
             riga = line.split(',')
 
             if riga [0] != 'date':
-                
-                if all('' == s or s.isspace() for s in riga[1]):
 
-                    riga[1] = 0
+             
+                
+                if all('' == s or s.isspace() for s in                  riga[1]):
+
+                       riga[1] = 0
 
                 try:
-
+                    
                     riga[1] = int (riga[1])
 
-                except : 
+                except ValueError : 
 
                     riga[1] = 0
 
-
-                else:
-
-                    riga[1] = riga[1]
-
+                if riga[1] < 0:
+                    riga[1] = -riga[1]
+                
+                try:
+                    datetime.datetime.strptime(riga[0], '%Y-%m')
+                except ValueError:
+                    raise ExamException ('sono presenti date non corrette')
+                 
                 my_list.append(riga)
 
-                time_stamp.append(riga[0][:4])
+                
+
+                time_stamp.append(riga[0])
+
+                
+
+
+        if not my_list:
+            raise ExamException('Lista non completa')
+
+    
 
         for x in range (len(my_list)-1):
             for h in range (x+1,len(my_list)):
                 if my_list[x][0] == my_list[h][0]:
-                    raise ExamException ('Ci sono dati duplicati nel file')
+                    raise ExamException ('Ci sono dati                      duplicati nel file')
 
         for x in range(len(time_stamp)-1):
             if time_stamp[x+1] < time_stamp[x]:
-                raise ExamException('Sequenza temporale non ordinata')
+                raise ExamException('Sequenza temporale                 non ordinata')
+
+     
+
 
 
         return my_list
 
-mesi = ["01","02","03","04","05","06","07","08","09","10","11","12"]
-anno = {"01":30, "03":107}
-
-def sistema_tutto_che_se_no_mi_arrabbio(mesi, anno):
-    ''' 'La funzione prende in argomento una lista per avere i mesi come chiave e un dizionario in cui inserire chiavi. Se non sono presenti, alza un errore. Se lo alza, metto 0.
-        1Se non alza un errore invece, metto in da_ritornare la chiave "mesi[i]" e ci associ il valore che c'è nella corrispondente di anno.'''
-        
-    
-    lista = []
-    da_ritornare = dict() #dizionario vuoto, {}
-    for i in range(12): #i lo uso come iteratore per tutto il processo
-        try: #chiamare una chiave che non esiste alza un KeyError. Metto un try.
-            da_ritornare[mesi[i]] = anno[mesi[i]] 
-            lista.append(anno[mesi[i]])
-        except: #Non ho quella chiave
-            #Devo mettere 0 in chiave che manca
-            da_ritornare[mesi[i]] = 0
-            lista.append(0)
-            
-    return lista
 
 
 
@@ -102,25 +106,25 @@ def detect_similar_monthly_variations(time_series, years):
 
     #controllo che la lista years non sia vuota
     if not years:
-        raise ExamException('Errore : la lista degli anni è vuota')
-    #controllo che la lista abbia il giusto numero di elementi
+        raise ExamException('la lista degli            anni è vuota')
+    #controllo che la lista abbia il giusto numero di       elementi
     if len(years) > 2 or len(years) < 2:
-        raise ExamException('Errore: la lista degli anni fornita non ha il numero corretto di elementi')
+        raise ExamException('la lista degli             anni fornita non ha il numero corretto di               elementi')
 
     # controllo che time_series sia una lista
     if not isinstance(time_series, list):
-        raise ExamException('Errore: time_series non è una lista di liste')
+        raise ExamException('time_series non è          una lista di liste')
 
     # Controllo che tutti gli anni siano numero interi:
     if not isinstance(years[0], int) or not isinstance(years[1], int):
-        raise ExamException('Errore gli anni forniti non sono numeri interi')
+        raise ExamException('gli anni forniti non sono numeri interi')
 
     #controllo che i due anni forniti siano differenti
 
     if years[0] == years[1]:
-        raise ExamException('Errore: i due anni devono essere diversi ! ')
+        raise ExamException('i due anni forniti devono essere diversi ! ')
 
-    # Il primo anno deve essere piu piccolo del secondo
+    # Il primo anno deve essere piu piccolo del secondo, in caso negativo scambio gli anni forniti
     if years[0] > years[1]:
         tmp = years[0]
         years[0] = years[1]
@@ -128,27 +132,27 @@ def detect_similar_monthly_variations(time_series, years):
 
     # Controllo che il primo anno sia presente nella lista time_series
     if years[0] < int(time_series[0][0][:4]):
-        raise ExamException('Errore: il primo anno non è presente nella lista')
+        raise ExamException('il primo anno fornito non è presente nella lista')
     #controllo che il secondo anno sia presente nella lista time_series
     if years[1] > int(time_series[-1][0][:4]):
-        raise ExamException('Errore: l\' anno finale non è presente nella lista ! ')
+        raise ExamException('il secondo anno fornito  non è presente nella lista ! ')
 
     # Controllo che gli anni siano consecutivi
     if years[1] != years[0] + 1:
-        raise ExamException('Errore: gli anni non sono consecutivi ')
+        raise ExamException('gli anni forniti non sono consecutivi ')
 
-
+    #creo tre liste vuote rispettivamente per, salvare le variazioni mensili tra mesi consecutivi del primo anno, .... del secondo anno e infine creare la lista contenente i True e False a richiesta
     lista_variaz_primo_anno = []
     lista_variaz_sec_anno = []
     lista_finale = []
+
+    #con una lista comprehension riempio due liste con i dati riguardanti il numero mensile di passeggeri 
 
     pass_primo_anno = [riga[1] for riga in time_series if riga[0][0:4] == str(years[0])]
 
     pass_sec_anno = [riga[1] for riga in time_series if riga[0][0:4] == str(years[1])]
 
-    for i in range(12):
-        pass_primo_anno[i] = int (pass_primo_anno[i])
-        pass_sec_anno[i] = int (pass_sec_anno[i])
+   #calcolo le variazioni del num dei pass per per il primo e il secondo anno forniti e li inserisco nelle apposite liste
 
     for i in range(11):
         
@@ -160,9 +164,11 @@ def detect_similar_monthly_variations(time_series, years):
 
         lista_variaz_sec_anno.append(variaz_mesi_sec_anno)
 
+        #verifico la similitudine tra le variazioni di mesi successivi e la pongo falsa se una delle due variazioni è invariata, riempio una lista con i True e False a seconda del caso
+
         diff = lista_variaz_primo_anno[i] - lista_variaz_sec_anno[i]
 
-        if diff == lista_variaz_primo_anno[i] or diff == lista_variaz_sec_anno[i]:
+        if variaz_mesi_primo_anno == pass_primo_anno[i] or variaz_mesi_primo_anno == -pass_primo_anno[i]:
             diff = 4
 
         if(diff > 2 or diff < -2 ):
@@ -172,12 +178,14 @@ def detect_similar_monthly_variations(time_series, years):
 
     return lista_finale
 
+
+
 time_series_file = CSVTimeSeriesFile(name='data.csv')
 time_series = time_series_file.get_data()
-years = [1949,1950]
+
+years = [1949, 1950]
+    
+   
 print(detect_similar_monthly_variations(time_series, years))
 
-   
-
-
-
+    
